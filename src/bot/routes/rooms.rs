46c9b5fe22a0656e1e0024a7f9c2da3ab009bebc
b-avb <brayan.vargas.dev@gmail.com>
2024-06-11 10:19:38 -0500

    feat: state event for industry & use buffer instead tmp file

--- src/bot/routes/rooms.rs
+++ src/bot/routes/rooms.rs
@@ -14,10 +14,11 @@ use matrix_sdk::ruma::{
 };
 use matrix_sdk::Client as MatrixClient;
 use reqwest::StatusCode;
+use ruma::api::client::state::get_state_events_for_key;
+use ruma::events::macros::EventContent;
 use ruma::events::room::avatar::RoomAvatarEventContent;
 use ruma::OwnedMxcUri;
-use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use serde::{Deserialize, Serialize};
 use crate::domain::{CommunityData, CommunityMatrixId, Uri};
 use crate::utils::{error_chain_fmt, mxc_to_download_uri};
 
@@ -58,7 +59,6 @@ pub async fn create(
     form: web::Json<CommunityData>,
     matrix_client: web::Data<MatrixClient>,
 ) -> Result<impl Responder, ServerError> {
-    println!("{:?}", form);
     let response = on_handle_create_room(form.0,&matrix_client)
         .await
         .context("Failed to create a room")?;
@@ -68,6 +68,12 @@ pub async fn create(
     }))
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize, EventContent)]
+#[ruma_event(type = "m.virto.industry", kind = State, state_key_type = String)]
+pub struct IndustryEventContent {
+    pub industry_name: String,
+}
+
 #[tracing::instrument(name = "Create a room", skip(matrix_client))]
 pub async fn on_handle_create_room(
     room: CommunityData, 
@@ -75,13 +81,10 @@ pub async fn on_handle_create_room(
 ) -> Result<String, Error> {
     let mut request = api::client::room::create_room::v3::Request::new();
     request.name = Some(&room.name);
-    
-    let topic = format!("{}\nTAG_VIRTO: {}", 
-        room.description.unwrap_or("".to_string()), 
-        room.industry 
-    );
+
+    let topic = room.description.unwrap_or("".to_string());
     request.topic = Some(&topic);
-    
+
     let mut init_state_ev_vec: Vec<Raw<AnyInitialStateEvent>> = vec![];
 
     if let Some(uri) = room.logo {
@@ -93,14 +96,25 @@ pub async fn on_handle_create_room(
             state_key: EmptyStateKey,
         };
 
-        let raw_init_state_ev =
-            Raw::new(&init_state_ev)?;
-
+        let raw_init_state_ev = Raw::new(&init_state_ev)?;
         let raw_any_init_state_ev: Raw<AnyInitialStateEvent> = raw_init_state_ev.cast();
         init_state_ev_vec.push(raw_any_init_state_ev);
+    }
 
-        request.initial_state = &init_state_ev_vec;
+    let custom_content = IndustryEventContent {
+        industry_name: room.industry,
     };
+
+    let custom_state_ev: InitialStateEvent<IndustryEventContent> = InitialStateEvent {
+        content: custom_content,
+        state_key: "".to_string(),
+    };
+
+    let raw_custom_state_ev = Raw::new(&custom_state_ev)?;
+    let raw_any_custom_state_ev: Raw<AnyInitialStateEvent> = raw_custom_state_ev.cast();
+    init_state_ev_vec.push(raw_any_custom_state_ev);
+
+    request.initial_state = &init_state_ev_vec;
 
     let mut content = CreationContent::new();
     content.room_type = Some(ruma::room::RoomType::Space);
@@ -119,32 +133,26 @@ pub async fn on_handle_create_room(
     skip(payload, matrix_client) 
 )]
 pub async fn upload(mut payload: Multipart, matrix_client: web::Data<MatrixClient>) -> Result<impl Responder, ServerError> {
-    let filepath = "./upload.tmp";
-    let mut f = File::create(filepath).await.map_err(|e| ServerError::UnexpectedError(e.into()))?;
+    let mut buffer = Vec::new();
 
     if let Some(field) = payload.next().await {
         let mut field = field.map_err(|_| ServerError::ValidationError("File not found".to_string()))?;
         while let Some(chunk) = field.next().await {
             let data = chunk.map_err(|_| ServerError::ValidationError("File not found".to_string()))?;
-            f.write_all(&data).await.map_err(|e| ServerError::UnexpectedError(e.into()))?;
+            buffer.extend_from_slice(&data);
         }
     }
 
-    let response = upload_to_matrix(filepath, &matrix_client).await?;
+    let response = upload_to_matrix(&buffer, &matrix_client).await?;
 
     Ok(web::Json(Uri {
         uri: response
     }))
 }
 
-#[tracing::instrument(name = "Upload a file to matrix", skip(filepath, matrix_client))]
-async fn upload_to_matrix(filepath: &str, matrix_client: &MatrixClient) -> Result<String, Error> {
-
-    let mut file = File::open(filepath).await?;
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).await?;
-
-    let response = matrix_client.media().upload(&mime::IMAGE_JPEG, &buffer).await?;
+#[tracing::instrument(name = "Upload a file to matrix", skip(buffer, matrix_client))]
+async fn upload_to_matrix(buffer: &[u8], matrix_client: &MatrixClient) -> Result<String, Error> {
+    let response = matrix_client.media().upload(&mime::IMAGE_JPEG, buffer).await?;
 
     Ok(response.content_uri.to_string())
 }
@@ -163,21 +171,28 @@ pub async fn get_by_id(path: web::Path<String>, matrix_client: web::Data<MatrixC
 #[tracing::instrument(name = "Get a space from matrix", skip(id, matrix_client))]
 async fn get_by_id_from_matrix(id: &str, matrix_client: &MatrixClient) -> Result<CommunityData, Error> {
     let room = RoomId::parse(id)?;
-    let response = matrix_client.get_room(&room).expect("Should get a matrix room id");
+    let request = ruma::api::client::space::get_hierarchy::v1::Request::new(&room);
+    let response = matrix_client.send(request, None).await?;
 
-    let name = response.name().unwrap_or("".to_string());
-    let logo = response.avatar_url().and_then(|uri| mxc_to_download_uri(&uri));
-    let mut description = None;
-    let mut industry = String::new();
+    let response = response.rooms.get(0).ok_or(anyhow::anyhow!("Room not found"))?.clone();
 
+    let name = response.name.unwrap_or("".to_string());
+    let logo = response.avatar_url.and_then(|uri| mxc_to_download_uri(&uri));
+    let description = response.topic;
+
+    let state_key = "".to_string();
+    let request = get_state_events_for_key::v3::Request::new(
+        &room,
+        "m.virto.industry".into(),
+        &state_key,
+    );
+
+    let response = matrix_client.send(request, None).await?;
+    let event = response.content;
+    let event_json = serde_json::to_string(&event)?;
+    let custom_event: IndustryEventContent = serde_json::from_str(&event_json)?;
     
-    if let Some(topic) = response.topic() {
-        tracing::Span::current().record("topic", &tracing::field::display(&topic));
-        let split = topic.split("TAG_VIRTO: ").collect::<Vec<&str>>();
-
-        description = split.get(0).and_then(|v| Some(v.to_string()));
-        industry = split.get(1).map(|v| v.to_string()).unwrap_or("".to_string());
-    }
+    let industry = custom_event.industry_name;
 
     let community = CommunityData { name, logo, description, industry };
 
